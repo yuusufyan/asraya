b6616dcce562c2157b73fd5d33d5fe1d970ee9f1
yuusufyan <yuusufyan13@gmail.com>
2026-06-22 13:16:36 +0700

    FEAT : initialize API entry point and integrate Fiber, GORM, and RabbitMQ dependencies

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,117 @@
 package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/sirupsen/logrus"
+	bootsrap "github.com/yuusufyan/asraya/internal/boostrap"
+	"github.com/yuusufyan/asraya/internal/config"
+	"github.com/yuusufyan/go-common/pkg/database"
+	"github.com/yuusufyan/go-common/pkg/logger"
+	commonMiddleware "github.com/yuusufyan/go-common/pkg/middleware/fiber"
+	"github.com/yuusufyan/go-common/pkg/rabbitmq"
+	"github.com/yuusufyan/go-common/pkg/utils"
+)
+
+type customLogger struct {
+	*logrus.Logger
+}
+
+func (l *customLogger) WithCtx(ctx context.Context) *logrus.Entry {
+	return logger.WithCtx(ctx, l.Logger)
+}
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		fallbackLog := logger.New(false)
+		fallbackLog.Fatalf("Failed to load config: %v", err)
+	}
+
+	log := logrus.New()
+	if cfg.IsProd {
+		log.SetFormatter(&logrus.JSONFormatter{
+			TimestampFormat: "2006-01-02T15:04:05.999Z07:00",
+		})
+		log.SetLevel(logrus.InfoLevel)
+	} else {
+		log.SetFormatter(&logrus.TextFormatter{
+			TimestampFormat: "15:04:05.000",
+			FullTimestamp:   true,
+			ForceColors:     true,
+		})
+		log.SetLevel(logrus.DebugLevel)
+	}
+
+	appLogger := &customLogger{Logger: log}
+
+	dbConfig := &database.DBConfig{
+		Host:     cfg.DBHost,
+		User:     cfg.DBUser,
+		Password: cfg.DBPassword,
+		DBName:   cfg.DBName,
+		Port:     cfg.DBPort,
+	}
+	db, err := database.Connect(dbConfig, appLogger, cfg.IsProd)
+	if err != nil {
+		log.Warnf("Database connection skipped or failed: %v", err)
+	} else {
+		log.Info("Database connection success")
+		db.Use(database.NewAuditPlugin())
+	}
+
+	var mq rabbitmq.RabbitMQClient
+	if cfg.RabbitMQURL != "" {
+		mq, err = rabbitmq.NewRabbitMQClient(cfg.RabbitMQURL)
+		if err != nil {
+			log.Errorf("Failed to connect to RabbitMQ: %v", err)
+		} else {
+			log.Info("Connected to RabbitMQ")
+		}
+	} else {
+		log.Info("RabbitMQ URL not provided, skipping connection")
+	}
+
+	fiberCfg := bootsrap.FiberConfig(cfg, appLogger.Logger)
+	app := fiber.New(fiberCfg)
+
+	app.Get("/health", utils.NewHealthHandler(db, nil))
+	commonMiddleware.InstallCommonMiddleware(app, appLogger)
+
+	go func() {
+		port := fmt.Sprintf("%s:%d", cfg.AppHost, cfg.AppPort)
+		log.Infof("Starting server on port %s", port)
+		if err := app.Listen(port); err != nil {
+			log.Fatalf("Error starting server: %v", err)
+		}
+	}()
+
+	sh := utils.NewShutdownHelper(log)
+	sh.Wait()
+
+	shutdownTasks := map[string]func(ctx context.Context) error{
+		"Fiber": func(ctx context.Context) error {
+			return app.ShutdownWithContext(ctx)
+		},
+	}
+
+	if db != nil {
+		shutdownTasks["Database"] = func(ctx context.Context) error {
+			sqlDB, err := db.DB()
+			if err != nil {
+				return err
+			}
+			return sqlDB.Close()
+		}
+	}
+
+	if mq != nil {
+		shutdownTasks["RabbitMQ"] = func(ctx context.Context) error {
+			return mq.Close()
+		}
+	}
+
+	sh.Graceful(shutdownTasks)
+}
--- go.mod
+++ go.mod
@@ -3,22 +3,60 @@ module github.com/yuusufyan/asraya
 go 1.25.5
 
 require (
+	github.com/bytedance/sonic v1.15.2
+	github.com/gofiber/fiber/v2 v2.52.13
+	github.com/sirupsen/logrus v1.9.4
 	github.com/spf13/viper v1.21.0
 	github.com/yuusufyan/go-common v0.0.0-20260521033627-4477051560ec
 )
 
 require (
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/bytedance/gopkg v0.1.3 // indirect
+	github.com/bytedance/sonic/loader v0.5.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/cloudwego/base64x v0.1.6 // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.13 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.30.2 // indirect
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
+	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.6.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
+	github.com/rabbitmq/amqp091-go v1.11.0 // indirect
+	github.com/redis/go-redis/v9 v9.19.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/sagikazarmark/locafero v0.11.0 // indirect
-	github.com/sirupsen/logrus v1.9.4 // indirect
 	github.com/sourcegraph/conc v0.3.1-0.20240121214520-5f936abd7ae8 // indirect
 	github.com/spf13/afero v1.15.0 // indirect
 	github.com/spf13/cast v1.10.0 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
+	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
+	golang.org/x/crypto v0.49.0 // indirect
+	golang.org/x/sync v0.20.0 // indirect
 	golang.org/x/sys v0.42.0 // indirect
 	golang.org/x/text v0.35.0 // indirect
+	gorm.io/driver/postgres v1.6.0 // indirect
+	gorm.io/gorm v1.31.1 // indirect
 )
